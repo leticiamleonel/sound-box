3746c8d999e2ed61b9f7a17a7ebf9d30d22280e1
leticiamleonel <leticiamoreira0318@gmail.com>
2024-05-18 08:24:27 -0300

    novo js

---
+++ main.js
@@ -0,0 +1,16 @@
+function tocaSom(idElementoAudio) {
+  document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll(".tecla");
+
+let contador = 0;
+
+while (contador < listaDeTeclas.length) {
+  listaDeTeclas[contador].onclick = function () {
+    tocaSom("#som_tecla_pom");
+  };
+  contador = contador + 1;
+
+  console.log(contador);
+}
